82ae2983a01f3130d2ee4a85178ea5cc099d8993
pavithramuddu93 <pavithramuddu93>
2022-09-21 14:50:11 +0530

    Added UC4: InsertINdex

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -27,8 +27,19 @@ public class LinkedList<K> {
 
 		}
 	}
+	public void addFirst(Object data) {
+		Node newNode = new Node(data);
 
-	// add last 
+		if (head == null)
+			head = newNode;
+		else {
+			Node temp = head;
+			head = newNode;
+			head.next = temp;
+
+		}
+	}
+
 	public void addLast(Object data) {
 		Node newNode = new Node(data);
 		if (head == null)
@@ -42,18 +53,78 @@ public class LinkedList<K> {
 		}
 	}
 
-	// Display all Node with Value
+	
 	public void display() {
-
-		if (head == null) {
-			System.out.println("List is empty");
-		}
-		Node temp = head;
-		while (temp != null) {
-			System.out.println(temp.data);
-			temp = temp.next;
+		if (head == null)
+			System.out.println("No elements to display...");
+		else if (head.next == null)
+			System.out.println(head.data);
+		else {
+			Node temp = head;
+			while (temp != null) {
+				if (temp.next != null)
+					System.out.print(temp.data + "==>");
+				else
+					System.out.print(temp.data + "\n");
+				temp = temp.next;
+			}
 		}
 	}
+
+	public int size() {
+		int count = 0;
+
+		Node temp = head;
+
+		while (temp != null) {
+			count++;
+			temp = temp.next;
+		}
+
+		return count;
+
+	}
+
+	public void insertIndex(int userIndex, Object data) {
+
+		// User trying to insert @ first position
+		if (userIndex == 0)
+			addFirst(data);
+
+		// User trying to insert @ last position
+		else if (userIndex == size())
+			addLast(data);
+
+		// User trying to insert @ invalid position
+		else if (userIndex < 0 || userIndex >= size())
+			System.out.println("Invalid index");
+
+		// User trying to insert @ specific index
+		else {
+			// Creating New Node
+			Node newNode = new Node(data);
+
+			// To track traversing
+			int index = 0;
+
+			// Pointers to track left & right side elements
+			Node left = head;
+			Node right = left.next;
+
+			// Traverse or Move till last element before user entered index
+			while (index < userIndex - 1) {
+				left = left.next;
+				right = right.next;
+				index++;
+			}
+			// Connecting new Node with right side elements
+			newNode.next = right;
+			
+			// Connecting left side elements with new Node
+			left.next = newNode;
+		}
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -61,8 +132,8 @@ public class LinkedList<K> {
 		LinkedList sc = new LinkedList();
 
 		sc.addLast(56);
-		sc.addLast(30);
 		sc.addLast(70);
+		sc.insertIndex(1,30);
 		sc.display();
 		
 	}
